d17102f7053582757deb08b3376a407a1f31330e
Anna Webber <anna.webber@disneystreaming.com>
2023-05-02 12:09:44 -0400

    new assignment

--- submission.py
+++ submission.py
@@ -1,105 +1,210 @@
-from typing import Dict, List, Tuple
 import math
+from typing import List
+
+class Node:
+
+  """
+  This class, Node, represents a single node in a decision tree. It is designed to store information about the tree
+  structure and the specific split criteria at each node. It is important to note that this class should NOT be
+  modified as it is part of the assignment and will be used by the autograder.
+
+  The attributes of the Node class are:
+  - split_dim: The dimension/feature along which the node splits the data (-1 by default, indicating uninitialized)
+  - split_point: The value used for splitting the data at this node (-1 by default, indicating uninitialized)
+  - label: The class label assigned to this node, which is the majority label of the data at this node. If there is a tie,
+    the numerically smaller label is assigned (-1 by default, indicating uninitialized)
+  - left: The left child node of this node (None by default). Either None or a Node object.
+  - right: The right child node of this node (None by default) Either None or a Node object.
+  """
+
+  def __init__(self):
+    self.split_dim = -1
+    self.split_point = -1
+    self.label = -1
+    self.left = None
+    self.right = None
+    self.depth = None
+
+  def _is_leaf(self):
+      if self.right == None and self.left == None:
+          return True
+      else: return False
+  
+  def to_str(self):
+      if self._is_leaf():
+          print_str = f"Depth {self.depth} Leaf Node {self.label} with split_dim = {self.split_dim} and split_point = {self.split_point}" 
+      else:
+          print_str = f"Depth {self.depth} Node {self.label} with split_dim = {self.split_dim} and split_point = {self.split_point}" 
+      return print_str
+
 
 class Solution:
-    def confusion_matrix(self, true_labels: List[int], pred_labels: List[int]) -> Dict[Tuple[int, int], int]:
-        """Calculate the confusion matrix and return it as a sparse matrix in dictionary form.
-        Args:
-          true_labels: list of true labels
-          pred_labels: list of predicted labels
-        Returns:
-          A dictionary of (true_label, pred_label): count
-        """
-        # matrix = {}
-        # matrix[(1, 1)] = matrix[(1, 0)] = matrix[(0, 1)] = matrix[(0, 0)] = 0
-        # for true, pred in zip(true_labels, pred_labels):
-        #     matrix[(true, pred)] += 1
 
-        matrix = {}
-        for true, pred in zip(true_labels, pred_labels):
-            if (true, pred) not in matrix:
-                matrix[(true, pred)] = 1
+  def entropy(self, p_pos: float) -> float:
+        if p_pos == 0 or p_pos == 1:
+            return 0
+        else:
+            return -p_pos * math.log2(p_pos) #- (1 - p_pos) * math.log2(1 - p_pos)
+
+  def split_info(self, data: List[List[float]], label: List[int], split_dim: int, split_point: float) -> float:
+      """
+      Compute the information needed to classify a dataset if it's split
+      with the given splitting dimension and splitting point, i.e. Info_A in the slides.
+
+      Parameters:
+      data (List[List]): A nested list representing the dataset.
+      label (List): A list containing the class labels for each data point.
+      split_dim (int): The dimension/attribute index to split the data on.
+      split_point (float): The value at which the data should be split along the given dimension.
+
+      Returns:
+      float: The calculated Info_A value for the given split. Do NOT round this value
+      """
+      n = len(data)
+      left_count = 0
+      left_label_counts = {}
+      right_count = 0
+      right_label_counts = {}
+
+      for i in range(n):
+          if data[i][split_dim] <= split_point:
+              left_count += 1
+              if label[i] in left_label_counts:
+                  left_label_counts[label[i]] += 1
+              else:
+                  left_label_counts[label[i]] = 1
+          else:
+              right_count += 1
+              if label[i] in right_label_counts:
+                  right_label_counts[label[i]] += 1
+              else:
+                  right_label_counts[label[i]] = 1
+
+      p_left = left_count / n
+      p_right = right_count / n
+
+      if left_count == 0 or right_count == 0:
+          return float(0)
+
+      left_entropy = sum([self.entropy(left_label_counts[label] / left_count) for label in left_label_counts])
+
+      right_entropy = sum([self.entropy(right_label_counts[label] / right_count) for label in right_label_counts])
+
+
+      return p_left * left_entropy + p_right * right_entropy
+
+  def split_node(self, node, data, labels, depth):
+        node.depth = depth
+
+        counts = [0] * len(range(0, max(labels)))
+        for label in labels:
+            counts[label-1] += 1
+    
+        node.label = max(range(len(counts)), key=counts.__getitem__) + 1
+
+        # If stopping condition met, set the node's label and return
+        if len(data) == 0 or depth >= self.max_depth:
+            return
+              
+        # Find the best feature to split the data
+        best_feature, best_split_point = None, None
+        best_info_gain = 0
+        features = range(len(data[0]))
+        for feature in features:
+            
+            a = sorted([data[i][feature] for i in range(len(data))])
+            split_points = set()
+            for i in range(len(a)-1):
+                midpoint = (a[i] + a[i+1]) / 2.0
+                split_points.add(midpoint)
+            
+            for split_point in split_points:
+                info_gain = self.split_info(data, labels, feature, split_point)
+                if info_gain > best_info_gain:
+                    best_info_gain = info_gain
+                    best_feature = feature
+                    best_split_point = split_point
+
+        if best_info_gain == 0:
+            return
+
+        # Split the data based on the best feature and split point
+        left_data, left_labels, right_data, right_labels = [], [], [], []
+        for i in range(len(data)):
+            if data[i][best_feature] <= best_split_point:
+                left_data.append(data[i])
+                left_labels.append(labels[i])
             else:
-                matrix[(true, pred)] += 1
+                right_data.append(data[i])
+                right_labels.append(labels[i])
 
-        sorted_keys = sorted(matrix.keys())
-        sorted_data = {k: matrix[k] for k in sorted_keys}
-        return sorted_data
+        # Create child nodes and recursively split them
+        node.split_dim = best_feature
+        node.split_point = best_split_point
+        node.left = Node()
+        node.right = Node()
+        self.split_node(node.left, left_data, left_labels, depth + 1)
+        self.split_node(node.right, right_data, right_labels, depth + 1)
 
+  def fit(self, train_data: List[List[float]], train_label: List[int]) -> None:
 
-    def confusion_mat_sums(self, confusion_mat) -> Tuple[List[int], List[int]]:
+    self.root = Node()
+    self.max_depth = 2
 
-        max_i, max_j = 0, 0
-        for i, j in confusion_mat.keys():
-            if i > max_i:
-                max_i = i
-            if j > max_j:
-                max_j = j
+    """
+    Fit the decision tree model using the provided training data and labels.
 
-        # print(max_i, max_j)
-        i_sums = [0] * (max_i + 1)
-        j_sums = [0] * (max_j + 1)
+    Parameters:
+    train_data (List[List[float]]): A nested list of floating point numbers representing the training data.
+    train_label (List[int]): A list of integers representing the class labels for each data point in the training set.
 
-        # print(confusion_mat)
-        for key in confusion_mat.keys():
-            # print('\n')
-            # print(key)
-            i, j = key 
-            val = confusion_mat[key]
-            # print(val)
-            i_sums[i] += val
-            # print(j_sums)
-            j_sums[j] += val
-            # print(j_sums)
-        # print('i_sums: {} | j_sums: {}'.format(i_sums, j_sums))
-        return i_sums, j_sums
+    This method initializes the decision tree model by creating the root node. It then builds the decision tree starting 
+    from the root node
+    
+    It is important to note that for tree structure evaluation, the autograder for this assignment
+    first calls this method. It then performs tree traversals starting from the root node in order to check whether 
+    the tree structure is correct. 
+    
+    So it is very important to ensure that self.root is assigned correctly to the root node
+    
+    It is best to use a different method (such as in the example above) to build the decision tree.
+    """
 
-    def jaccard(self, true_labels: List[int], pred_labels: List[int]) -> float:
-        """Calculate the Jaccard index.
-        Args:
-        true_labels: list of true cluster labels
-        pred_labels: list of predicted cluster labels
-        Returns:
-        The Jaccard index. Do NOT round this value.
-        """
-        confusion_mat = self.confusion_matrix(true_labels, pred_labels)
-        i_sums, j_sums = self.confusion_mat_sums(confusion_mat)
+    # Call the recursive method to build the decision tree
+    self.split_node(self.root, train_data, train_label, depth=0)
 
-        # m = sum(i_sums)
+  def classify(self, train_data: List[List[float]], train_label: List[int], test_data: List[List[float]]) -> List[int]:
+    """
+    Classify the test data using a decision tree model built from the provided training data and labels.
+    This method first fits the decision tree model using the provided training data and labels by calling the
+    'fit()' method.
 
-        tp = fp = fn = tn = 0
-        tp = sum(nij*(nij-1)/2 for nij in confusion_mat.values() if nij > 0)
-        fp = sum(ni*(ni-1)/2 for ni in i_sums if ni > 0) - tp
-        fn = sum(nj*(nj-1)/2 for nj in j_sums if nj > 0) - tp
-        # tn = m*(m-1)/2 - tp - fn - fp
+    Parameters:
+    train_data (List[List[float]]): A nested list of floating point numbers representing the training data.
+    train_label (List[int]): A list of integers representing the class labels for each data point in the training set.
+    test_data (List[List[float]]): A nested list of floating point numbers representing the test data.
 
-        # print("j = {} / ({} + {} + {})".format(tp,tp,fn,fp))
-        return (tp / (tp + fn + fp))
+    Returns:
+    List[int]: A list of integer predictions, which are the label predictions for the test data after fitting
+               the train data and labels to a decision tree.
+    """
+    # Fit the decision tree model using the provided training data and labels
+    self.fit(train_data, train_label)
 
-    def nmi(self, true_labels: List[int], pred_labels: List[int]) -> float:
-        """Calculate the normalized mutual information.
-        Args:
-          true_labels: list of true cluster labels
-          pred_labels: list of predicted cluster labels
-        Returns:
-          The normalized mutual information. Do NOT round this value.
-        """
-        confusion_mat = self.confusion_matrix(true_labels, pred_labels)
-        i_sums, j_sums = self.confusion_mat_sums(confusion_mat)
+    # Initialize an empty list to store the predicted labels for the test data
+    predicted_labels = []
 
-        n = sum(confusion_mat.values())
-        p_i = [i / n for i in i_sums]
-        p_j = [j / n for j in j_sums]
+    # Traverse the decision tree and predict the label for each test data point
+    for point in test_data:
+        current_node = self.root
+        while not current_node._is_leaf:
+            # Determine which child node to traverse based on the split condition
+            if point[current_node.split_dim] <= current_node.split_point:
+                current_node = current_node.left_child
+            else:
+                current_node = current_node.right_child
 
-        mi = 0
-        for key in confusion_mat.keys():
-            i, j = key
-            pij = confusion_mat[key] / n
-            if pij > 0:
-                mi += pij * math.log(pij / (p_i[i] * p_j[j]))
+        # Add the predicted label to the list of predicted labels
+        predicted_labels.append(current_node.label)
 
-        h_i = -sum(pi * math.log(pi) if pi > 0 else 0 for pi in p_i)
-        h_j = -sum(pj * math.log(pj) if pj > 0 else 0 for pj in p_j)
-
-        nmi = mi / (math.sqrt(h_i*h_j))
-        return nmi
+    return predicted_labels
