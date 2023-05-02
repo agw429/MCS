d89ec4d554eecb28c6b0d27a6e887cd14a383798
Anna Webber <anna.webber@disneystreaming.com>
2023-05-02 14:48:53 -0400

    new try

--- submission.py
+++ submission.py
@@ -1,210 +1,47 @@
-import math
 from typing import List
+from submission import Solution
 
-class Node:
-
-  """
-  This class, Node, represents a single node in a decision tree. It is designed to store information about the tree
-  structure and the specific split criteria at each node. It is important to note that this class should NOT be
-  modified as it is part of the assignment and will be used by the autograder.
-
-  The attributes of the Node class are:
-  - split_dim: The dimension/feature along which the node splits the data (-1 by default, indicating uninitialized)
-  - split_point: The value used for splitting the data at this node (-1 by default, indicating uninitialized)
-  - label: The class label assigned to this node, which is the majority label of the data at this node. If there is a tie,
-    the numerically smaller label is assigned (-1 by default, indicating uninitialized)
-  - left: The left child node of this node (None by default). Either None or a Node object.
-  - right: The right child node of this node (None by default) Either None or a Node object.
-  """
-
-  def __init__(self):
-    self.split_dim = -1
-    self.split_point = -1
-    self.label = -1
-    self.left = None
-    self.right = None
-    self.depth = None
-
-  def _is_leaf(self):
-      if self.right == None and self.left == None:
-          return True
-      else: return False
-  
-  def to_str(self):
-      if self._is_leaf():
-          print_str = f"Depth {self.depth} Leaf Node {self.label} with split_dim = {self.split_dim} and split_point = {self.split_point}" 
-      else:
-          print_str = f"Depth {self.depth} Node {self.label} with split_dim = {self.split_dim} and split_point = {self.split_point}" 
-      return print_str
-
-
-class Solution:
-
-  def entropy(self, p_pos: float) -> float:
-        if p_pos == 0 or p_pos == 1:
-            return 0
+# read input file
+n = "01"
+data = {'train': [], 'test': []}
+with open(f'PA-Week11/sample_test_cases/tree_structure/input{n}.txt') as f:
+    for line in f:
+        items = line.strip().split()
+        label = int(items[0])
+        features = {int(item.split(':')[0]): float(item.split(':')[1]) for item in items[1:]}
+        if label == -1:
+            data['test'].append(features)
         else:
-            return -p_pos * math.log2(p_pos) #- (1 - p_pos) * math.log2(1 - p_pos)
+            data['train'].append((features, label))
 
-  def split_info(self, data: List[List[float]], label: List[int], split_dim: int, split_point: float) -> float:
-      """
-      Compute the information needed to classify a dataset if it's split
-      with the given splitting dimension and splitting point, i.e. Info_A in the slides.
+# instantiate solution class
+clf = Solution()
 
-      Parameters:
-      data (List[List]): A nested list representing the dataset.
-      label (List): A list containing the class labels for each data point.
-      split_dim (int): The dimension/attribute index to split the data on.
-      split_point (float): The value at which the data should be split along the given dimension.
+# train the model
+train_data = [x[0] for x in data['train']]
+train_label = [x[1] for x in data['train']]
+test_data = [x[0] for x in data['test']]
 
-      Returns:
-      float: The calculated Info_A value for the given split. Do NOT round this value
-      """
-      n = len(data)
-      left_count = 0
-      left_label_counts = {}
-      right_count = 0
-      right_label_counts = {}
-
-      for i in range(n):
-          if data[i][split_dim] <= split_point:
-              left_count += 1
-              if label[i] in left_label_counts:
-                  left_label_counts[label[i]] += 1
-              else:
-                  left_label_counts[label[i]] = 1
-          else:
-              right_count += 1
-              if label[i] in right_label_counts:
-                  right_label_counts[label[i]] += 1
-              else:
-                  right_label_counts[label[i]] = 1
-
-      p_left = left_count / n
-      p_right = right_count / n
-
-      if left_count == 0 or right_count == 0:
-          return float(0)
-
-      left_entropy = sum([self.entropy(left_label_counts[label] / left_count) for label in left_label_counts])
-
-      right_entropy = sum([self.entropy(right_label_counts[label] / right_count) for label in right_label_counts])
+predictions = clf.fit(train_data, train_label)
 
 
-      return p_left * left_entropy + p_right * right_entropy
 
-  def split_node(self, node, data, labels, depth):
-        node.depth = depth
+# print(f"We predict {predictions}")
 
-        counts = [0] * len(range(0, max(labels)))
-        for label in labels:
-            counts[label-1] += 1
-    
-        node.label = max(range(len(counts)), key=counts.__getitem__) + 1
 
-        # If stopping condition met, set the node's label and return
-        if len(data) == 0 or depth >= self.max_depth:
-            return
-              
-        # Find the best feature to split the data
-        best_feature, best_split_point = None, None
-        best_info_gain = 0
-        features = range(len(data[0]))
-        for feature in features:
-            
-            a = sorted([data[i][feature] for i in range(len(data))])
-            split_points = set()
-            for i in range(len(a)-1):
-                midpoint = (a[i] + a[i+1]) / 2.0
-                split_points.add(midpoint)
-            
-            for split_point in split_points:
-                info_gain = self.split_info(data, labels, feature, split_point)
-                if info_gain > best_info_gain:
-                    best_info_gain = info_gain
-                    best_feature = feature
-                    best_split_point = split_point
+with open(f'PA-Week11/sample_test_cases/tree_structure/output{n}.txt') as f:
+    preorder = f.readline().split('}')
+    inorder = f.readline().split('}')
 
-        if best_info_gain == 0:
-            return
+print(f"Preorder Expected")
+for i in preorder:
+    print(i)
+print(f"Preorder Observed")
+clf.root.pre_order(clf.root)
 
-        # Split the data based on the best feature and split point
-        left_data, left_labels, right_data, right_labels = [], [], [], []
-        for i in range(len(data)):
-            if data[i][best_feature] <= best_split_point:
-                left_data.append(data[i])
-                left_labels.append(labels[i])
-            else:
-                right_data.append(data[i])
-                right_labels.append(labels[i])
+print(f"\nInorder Expected")
+for i in inorder:
+    print(i)
+print(f"Inorder Observed")
+clf.root.in_order(clf.root)
 
-        # Create child nodes and recursively split them
-        node.split_dim = best_feature
-        node.split_point = best_split_point
-        node.left = Node()
-        node.right = Node()
-        self.split_node(node.left, left_data, left_labels, depth + 1)
-        self.split_node(node.right, right_data, right_labels, depth + 1)
-
-  def fit(self, train_data: List[List[float]], train_label: List[int]) -> None:
-
-    self.root = Node()
-    self.max_depth = 2
-
-    """
-    Fit the decision tree model using the provided training data and labels.
-
-    Parameters:
-    train_data (List[List[float]]): A nested list of floating point numbers representing the training data.
-    train_label (List[int]): A list of integers representing the class labels for each data point in the training set.
-
-    This method initializes the decision tree model by creating the root node. It then builds the decision tree starting 
-    from the root node
-    
-    It is important to note that for tree structure evaluation, the autograder for this assignment
-    first calls this method. It then performs tree traversals starting from the root node in order to check whether 
-    the tree structure is correct. 
-    
-    So it is very important to ensure that self.root is assigned correctly to the root node
-    
-    It is best to use a different method (such as in the example above) to build the decision tree.
-    """
-
-    # Call the recursive method to build the decision tree
-    self.split_node(self.root, train_data, train_label, depth=0)
-
-  def classify(self, train_data: List[List[float]], train_label: List[int], test_data: List[List[float]]) -> List[int]:
-    """
-    Classify the test data using a decision tree model built from the provided training data and labels.
-    This method first fits the decision tree model using the provided training data and labels by calling the
-    'fit()' method.
-
-    Parameters:
-    train_data (List[List[float]]): A nested list of floating point numbers representing the training data.
-    train_label (List[int]): A list of integers representing the class labels for each data point in the training set.
-    test_data (List[List[float]]): A nested list of floating point numbers representing the test data.
-
-    Returns:
-    List[int]: A list of integer predictions, which are the label predictions for the test data after fitting
-               the train data and labels to a decision tree.
-    """
-    # Fit the decision tree model using the provided training data and labels
-    self.fit(train_data, train_label)
-
-    # Initialize an empty list to store the predicted labels for the test data
-    predicted_labels = []
-
-    # Traverse the decision tree and predict the label for each test data point
-    for point in test_data:
-        current_node = self.root
-        while not current_node._is_leaf:
-            # Determine which child node to traverse based on the split condition
-            if point[current_node.split_dim] <= current_node.split_point:
-                current_node = current_node.left_child
-            else:
-                current_node = current_node.right_child
-
-        # Add the predicted label to the list of predicted labels
-        predicted_labels.append(current_node.label)
-
-    return predicted_labels
